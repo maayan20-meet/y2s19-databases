798a65e8e8d2a5009b01035d07fca2a561278ffe
myname21-meet <myname21@meet.mit.edu>
2019-07-29 07:16:52 -0400

    finishid all the lab

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,20 +8,44 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(topic, title, rating):
+	arc_obj = Knowledge(
+		arc_topic=topic,
+		arc_title=title,
+		arc_rate=rating)
+	session.add(arc_obj)
+	session.commit()
 
 def query_all_articles():
-	pass
+	print(session.query(Knowledge).all())
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(topic):
+	article = session.query(Knowledge).filter_by(
+		arc_topic=topic).first()
+	return article
 
-def delete_article_by_topic():
-	pass
+def query_by_low_rate(threshold):
+	articles = session.query(Knowledge).filter_by(
+		arc_rate<threshold).all()
+	return articles
+
+def delete_article_by_topic(topic):
+	session.query(Knowledge).filter_by(arc_topic=topic).delete()
+	session.commit()
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+	session.commit()
 
-def edit_article_rating():
-	pass
+def edit_article_rating(new_rate, title):
+	articles = session.query(Knowledge).filter_by(arc_title=title).all()
+	for article in articles:
+		article.arc_rate = new_rate
+	session.commit()
+
+add_article("123", "abc", 9)
+add_article("123", "abc", 9)
+add_article("123", "abc", 9)
+add_article("123", "abc", 9)
+edit_article_rating(8, "abc")
+print(query_all_articles())
--- knowledge_model.py
+++ knowledge_model.py
@@ -13,5 +13,19 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__ = 'knowledge'
+	arc_id = Column(Integer, primary_key=True)
+	arc_topic = Column(String)
+	arc_title = Column(String)
+	arc_rate = Column(Integer)
 
-	pass
+	def __repr__(self):
+		if self.arc_rate > 7:
+			return ("If you want to learn about {}, you should look at the Wikipedia article called {}. We gave this article a rating of {} out of 10! The ID of this instance is {}").format(
+						self.arc_topic,
+						self.arc_title,
+						self.arc_rate,
+						self.arc_id)
+
+		else:
+			return "Unfortunately, this article does not have a better rating. Maybe, this is an article that should be replaced soon!"
